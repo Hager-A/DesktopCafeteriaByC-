ba82276628febdeb7f7da062e6aecce4c51e121f
AmiraFathalbab <amira.fathalbab@gmail.com>
2025-06-10 00:30:21 +0300

    design emp form(first edition)

--- WinFormsApp1/Emp.Designer.cs
+++ WinFormsApp1/Emp.Designer.cs
@@ -32,72 +32,115 @@
             dataGridView2 = new DataGridView();
             radioButton1 = new RadioButton();
             radioButton2 = new RadioButton();
-            radioButton3 = new RadioButton();
+            drinksBtn = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
+            dataGridView1.BackgroundColor = SystemColors.Info;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(488, 45);
+            dataGridView1.Location = new Point(466, 37);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(300, 268);
+            dataGridView1.Size = new Size(198, 246);
             dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
             // 
+            dataGridView2.BackgroundColor = SystemColors.Info;
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(28, 62);
+            dataGridView2.Location = new Point(24, 59);
+            dataGridView2.Margin = new Padding(3, 2, 3, 2);
             dataGridView2.Name = "dataGridView2";
             dataGridView2.RowHeadersWidth = 51;
-            dataGridView2.Size = new Size(357, 336);
+            dataGridView2.Size = new Size(312, 252);
             dataGridView2.TabIndex = 1;
             // 
             // radioButton1
             // 
             radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(304, 32);
+            radioButton1.Location = new Point(275, 24);
+            radioButton1.Margin = new Padding(3, 2, 3, 2);
             radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(117, 24);
+            radioButton1.Size = new Size(61, 19);
             radioButton1.TabIndex = 2;
             radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
+            radioButton1.Text = "Sweets";
             radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(166, 32);
+            radioButton2.Location = new Point(158, 24);
+            radioButton2.Margin = new Padding(3, 2, 3, 2);
             radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(117, 24);
+            radioButton2.Size = new Size(52, 19);
             radioButton2.TabIndex = 3;
             radioButton2.TabStop = true;
-            radioButton2.Text = "radioButton2";
+            radioButton2.Text = "Food";
             radioButton2.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // drinksBtn
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(28, 32);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(117, 24);
-            radioButton3.TabIndex = 4;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "radioButton3";
-            radioButton3.UseVisualStyleBackColor = true;
+            drinksBtn.AutoSize = true;
+            drinksBtn.Location = new Point(34, 24);
+            drinksBtn.Margin = new Padding(3, 2, 3, 2);
+            drinksBtn.Name = "drinksBtn";
+            drinksBtn.Size = new Size(58, 19);
+            drinksBtn.TabIndex = 4;
+            drinksBtn.TabStop = true;
+            drinksBtn.Text = "Drinks";
+            drinksBtn.UseVisualStyleBackColor = true;
+            drinksBtn.CheckedChanged += drinksBtn_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(466, 303);
+            button1.Name = "button1";
+            button1.Size = new Size(58, 23);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(545, 303);
+            button2.Name = "button2";
+            button2.Size = new Size(52, 23);
+            button2.TabIndex = 6;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(613, 303);
+            button3.Name = "button3";
+            button3.Size = new Size(51, 23);
+            button3.TabIndex = 7;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
             // 
             // Emp
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(radioButton3);
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(700, 338);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(drinksBtn);
             Controls.Add(radioButton2);
             Controls.Add(radioButton1);
             Controls.Add(dataGridView2);
             Controls.Add(dataGridView1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Emp";
             Text = "Emp";
             Load += Emp_Load;
@@ -113,6 +156,9 @@
         private DataGridView dataGridView2;
         private RadioButton radioButton1;
         private RadioButton radioButton2;
-        private RadioButton radioButton3;
+        private RadioButton drinksBtn;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- WinFormsApp1/Emp.cs
+++ WinFormsApp1/Emp.cs
@@ -22,7 +22,15 @@ namespace WinFormsApp1
 
         private void Emp_Load(object sender, EventArgs e)
         {
+            Text = $" Employee ";
+        }
 
+        private void drinksBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (drinksBtn.Checked)
+            {
+                dataGridView2.
+            }
         }
     }
 }
